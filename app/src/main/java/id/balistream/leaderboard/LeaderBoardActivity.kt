59f456b3ef9acf56f278316cf2f0aede100e9042
awidiyadew <awidiya.dewa@gmail.com>
2018-01-28 22:38:55 +0700

    setup project

---
+++ app/src/main/java/id/balistream/leaderboard/LeaderBoardActivity.kt
@@ -0,0 +1,12 @@
+package id.balistream.leaderboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learder_board)
+    }
+}
