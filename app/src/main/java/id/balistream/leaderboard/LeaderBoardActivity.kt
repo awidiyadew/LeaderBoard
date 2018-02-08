2b2f2fc109fac2795dfe4511125e12a7235bf24f
awidiyadew <awidiya.dewa@gmail.com>
2018-02-08 20:35:21 +0700

    add recyclerview and firestore library

--- app/src/main/java/id/balistream/leaderboard/LeaderBoardActivity.kt
+++ app/src/main/java/id/balistream/leaderboard/LeaderBoardActivity.kt
@@ -1,12 +1,28 @@
 package id.balistream.leaderboard
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LeaderBoardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learder_board)
+        val db = FirebaseFirestore.getInstance()
+        db.collection("players")
+                .addSnapshotListener({ values, error ->
+                    if (error != null) {
+                        Log.d("TAG", error.message)
+                        return@addSnapshotListener
+                    }
+
+                    values.forEach {
+                        val player = it.toObject(Player::class.java)
+                        Log.d("TAG", player.toString())
+                    }
+
+                })
     }
 }
---
+++ app/src/main/java/id/balistream/leaderboard/Player.kt
@@ -0,0 +1,8 @@
+package id.balistream.leaderboard
+
+data class Player(
+        val nik: Number,
+        val name: String,
+        val photo: String,
+        val score: Number
+)
